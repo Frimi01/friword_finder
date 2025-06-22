26e0bd272e2f33349e179c8768226aff7fe27124
Frimi01 <82447464+Frimi01@users.noreply.github.com>
2025-06-22 12:21:39 +0200

    started on adding back features from python version and changed to use .txt instead of java

--- src/main/java/com/Frimi01/Main.java
+++ src/main/java/com/Frimi01/Main.java
@@ -4,6 +4,11 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
 @Command(
         name = "friword-finder",
         mixinStandardHelpOptions = true,
@@ -18,13 +23,33 @@ public class Main implements Runnable {
     @Parameters(index = "1", description = "Something to print")
     private String printable;
 
+    @Parameters(index = "2", description = "Action: PrintAll, FindWord")
+    private String action;
+
+    @Parameters(index = "3", description = "Value for action")
+private String value;
+
     public static void main(String[] args) {
         int exitCode = new CommandLine(new Main()).execute(args);
         System.exit(exitCode);
     }
 
+    public static Set<String> loadDictionaryTxt(String path) {
+        try {
+            return new HashSet<>(Files.readAllLines(Paths.get(path)));
+        } catch (IOException e) {
+            System.err.println("Error: Dictionary text file not found or unreadable!");
+            System.exit(1);
+        }
+        return Collections.emptySet();
+    }
+
+
+
     @Override
     public void run() {
+        Set<String> Words = loadDictionaryTxt("assets/words.txt");
+
         switch (greeting.toLowerCase()) {
             case "hello":
                 System.out.println("Hello, world!");
@@ -37,5 +62,13 @@ public class Main implements Runnable {
                 System.out.println("Please use 'hello' or 'goodbye'");
         }
         System.out.println(printable);
+
+        switch (action.toLowerCase()) {
+            case "printall":
+                System.out.println(Words);
+                break;
+            case "findword":
+                System.out.println(Words.contains(value.toLowerCase()));
+        }
     }
 }
